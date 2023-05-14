a64b3a5b216c325d0ae53129e662fcad92dbe5e2
Porfirio Mendez Ocampo <porfiriomendez76@gmail.com>
2023-05-14 18:25:30 +0000

    implemented sum of arrays

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let result = 0;
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      result += sumItems(element);
+    } else {
+      result += element;
+    }
+    
+  }
+  return result;
 }
 
 module.exports = sumItems;
